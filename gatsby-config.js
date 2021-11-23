f36c8f1793ca5c54d1a7b1bb95726eede1ccee7e
Nick Dunn_C <nick.dunn@ndunn-12909.local>
2021-11-23 14:07:49 +1100

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "My Gatsby Site",
+    title: "Dowdy's Golf",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options:{
+        name: `blog`,
+        path: `${__dirname}/blog`
+      }
+    },
   ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import * as React from "react"
-import {Link} from "gatsby"
 import Layout from "../components/layout"
-import { StaticImage } from "gatsby-plugin-image"
+import {StaticImage} from "gatsby-plugin-image"
 
 // styles
 const pageStyles = {
